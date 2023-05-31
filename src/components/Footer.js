64e6edd90a3497287ef260dfe11d1bc024e05b7e
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-30 22:03:13 -0600

    finished the Footer.js starting Header.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,3 +29,45 @@ const socials = [
       color: "white",
     },
   ];
+
+  const Footer = () => {
+    return (
+      <footer className="footer bg-dark text-white">
+        <div className="footer-container">
+          <section className="social-icons-wrapper d-flex flex-md-row justify-content-center">
+            {socials.map(({ name, url, icon, color }) => (
+              <div
+                key={name}
+                className="d-flex flex-column align-items-center mx-md-2 transparent-bg"
+              >
+                <a
+                  href={url}
+                  className={`icon ${name}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={icon}
+                    bounce
+                    className="footer-icon-size" 
+                    style={{ color: color }}
+                  />
+                </a>
+                <a href={url} target="_blank" rel="noreferrer">
+                  <span className="d-inline-block mx-1 name">{name}</span>
+                </a>
+              </div>
+            ))}
+          </section>
+          <section className="form-footer">
+          <h5 className="designer-text">Designed with ⏳ by Thomas Calle</h5>
+            <p>
+            <h6 className="copyright-text">&copy; {new Date().getFullYear()} All rights reserved.</h6>
+            </p>
+          </section>
+        </div>
+      </footer>
+    );
+  };
+  // Export Footer
+  export default Footer;
