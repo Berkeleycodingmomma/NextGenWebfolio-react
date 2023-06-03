14083dcf63caaa04e4a94ca82ad3604a57127b62
Amanda Gray <floridabelle2021@protonmail.com>
2023-06-02 22:41:32 -0600

    added comments to my nav.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,49 +4,57 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-    return (
-      // Bootstrap Navbar
-      <Navbar 
-        bg="dark" 
-        variant="dark" 
-        expand="lg" 
-        sticky="top" 
-        className="py-0.5 mt-0">
-        <Container>
-            {/* <Navbar.Brand as={Link} to="/Berkeleycodingmomma/NextGenWebfolio-react/">Amanda Gray</Navbar.Brand> */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-              <Nav.Link 
-                  as={Link} 
-                  to="/Berkeleycodingmomma/NextGenWebfolio-react/about" 
-                  className="fs-5 nav-link-highlight shadow-lg">
-                    About Me
-                </Nav.Link>
-                <Nav.Link 
-                  as={Link} 
-                  to="/Berkeleycodingmomma/NextGenWebfolio-react/portfolio" 
-                  className="fs-5 nav-link-highlight shadow-lg">
-                    Portfolio
-                </Nav.Link>
-                <Nav.Link 
-                  as={Link} 
-                  to="/Berkeleycodingmomma/NextGenWebfolio-react/resume" className="fs-5 nav-link-highlight shadow-lg ">
-                    Resume
-                </Nav.Link>
-                <Nav.Link 
-                  as={Link} 
-                  to="/Berkeleycodingmomma/NextGenWebfolio-react/contact" 
-                  className="fs-5 nav-link-highlight shadow-lg ">
-                    Contact
-                </Nav.Link>
-                
-              </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  // Export Navigation
-  export default Navigation;
-  
+  return (
+    // Bootstrap Navbar
+    <Navbar 
+      bg="dark" 
+      variant="dark" 
+      expand="lg" 
+      sticky="top" 
+      className="py-0.5 mt-0">
+      <Container>
+        {/* Navbar Brand */}
+        {/* <Navbar.Brand as={Link} to="/Berkeleycodingmomma/NextGenWebfolio-react/">Amanda Gray</Navbar.Brand> */}
+        {/* Navbar Toggle */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* Navbar Collapse */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Navigation Links */}
+          <Nav className="ms-auto">
+            {/* About Me Link */}
+            <Nav.Link 
+              as={Link} 
+              to="/Berkeleycodingmomma/NextGenWebfolio-react/about" 
+              className="fs-5 nav-link-highlight shadow-lg">
+              About Me
+            </Nav.Link>
+            {/* Portfolio Link */}
+            <Nav.Link 
+              as={Link} 
+              to="/Berkeleycodingmomma/NextGenWebfolio-react/portfolio" 
+              className="fs-5 nav-link-highlight shadow-lg">
+              Portfolio
+            </Nav.Link>
+            {/* Resume Link */}
+            <Nav.Link 
+              as={Link} 
+              to="/Berkeleycodingmomma/NextGenWebfolio-react/resume" 
+              className="fs-5 nav-link-highlight shadow-lg ">
+              Resume
+            </Nav.Link>
+            {/* Contact Link */}
+            <Nav.Link 
+              as={Link} 
+              to="/Berkeleycodingmomma/NextGenWebfolio-react/contact" 
+              className="fs-5 nav-link-highlight shadow-lg ">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+// Export Navigation
+export default Navigation;
