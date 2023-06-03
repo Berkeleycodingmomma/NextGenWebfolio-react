1b7acf31650ec47bdab0931c5fcb7c00d0be647b
Amanda Gray <floridabelle2021@protonmail.com>
2023-06-02 22:41:17 -0600

    added comments to my header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,27 @@ import Navigation from "./Navigation";
 import { Container, Row, Col } from "react-bootstrap";
 import "../style/Header.css";
 
-
 function Header() {
-    return (
-      <>
-        <Container fluid className="bg-dark">
-          <Row className="justify-content-center text-center mb-0">
-            <Col className="pb-0">
-              <h1 className="text" style={{ fontSize: "70px", color: "white"}}>
-                <Link
-                  to="/Berkeleycodingmomma/NextGenWebfolio-react/"
-                  className="name-link">
-                   {"<>"}<span className="name">amanda_gray</span>{"</>"}
-                </Link>
-              </h1>
-            </Col>
-          </Row>
-        </Container>
-        <Navigation />
-      </>
-    );
-  }
+  return (
+    <>
+      {/* Header container */}
+      <Container fluid className="bg-dark">
+        <Row className="justify-content-center text-center mb-0">
+          <Col className="pb-0">
+            {/* Page title */}
+            <h1 className="text" style={{ fontSize: "70px", color: "white" }}>
+              {/* Link to homepage */}
+              <Link to="/Berkeleycodingmomma/NextGenWebfolio-react/" className="name-link">
+                {"<>"}<span className="name">amanda_gray</span>{"</>"}
+              </Link>
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+      {/* Navigation component */}
+      <Navigation />
+    </>
+  );
+}
 
 export default Header;
